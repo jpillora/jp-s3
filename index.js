91ebb0a2b0ccf6ee856b97c61f67b1a7ee166ae8
Jaime Pillora <dev@jpillora.com>
2020-02-02 23:22:16 +1100

    throw if no global client

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 //expose client
 exports.Client = require("./client");
 //make a global client
-if (process.env.BUCKET) {
-  const bucket = process.env.BUCKET;
-  const c = new exports.Client({ bucket });
-  for (let k of Object.getOwnPropertyNames(exports.Client.prototype)) {
-    let v = c[k];
-    if (typeof v === "function") {
-      exports[k] = v;
-    }
+const bucket = process.env.BUCKET;
+const c = bucket ? new exports.Client({ bucket }) : null;
+for (let k of Object.getOwnPropertyNames(exports.Client.prototype)) {
+  if (c && c[k] === "function") {
+    exports[k] = c[k];
+  } else {
+    exports[k] = () => {
+      throw `must set BUCKET env var for use module global ${k}()`;
+    };
   }
 }
